0e2812df4bc27a91b88c8bfaedf98b0eae9a8738
MateusOrlandinDias <mateus.orlandin@gmail.com>
2024-01-22 13:03:46 -0300

    FEAT general error handling Main

--- Main.py
+++ Main.py
@@ -1,6 +1,17 @@
 import PyRPAFramework as PyRPA
 from SecondaryWorkflows import TopwayCloud, TopwayEdu, Asana
+import GlobalEmail as ge
 
 config = PyRPA.InitAllSettings()
 
-TopwayCloud.topwayCloudLogin(config)
+try:
+    TopwayCloud.topwayCloudLogin(config)
+except Exception as e:
+    print(f"General Exception Handling error detected: {str(e)}")
+    ge.send_email(
+        '[R01 Personal - EnglishClassesMapper] Process Error',
+        f"Error found while running the automation:<br>{str(e)}",
+        str(config["EMAIL_SUSTAIN"]),
+        str(config["ROBOT_EMAIL_SENDER"]),
+        str(config["EMAIL_ROBOT_PASSWORD"]),
+    )
